387dd236ecb31cdf968be271798a538a81a8593a
abeob1 <sapb1.abeoelectra@gmail.com>
2017-05-16 14:06:14 +0800

    Web Service change by vivek

--- 1. Source/OutletManager_Service/AE_OutletManagerService_V001/AE_OutletManagerService_V001/Master.asmx.cs
+++ 1. Source/OutletManager_Service/AE_OutletManagerService_V001/AE_OutletManagerService_V001/Master.asmx.cs
@@ -90,26 +90,92 @@ namespace AE_OutletManagerService_V001
             }
         }
 
-        //Vikrant - 26/04/2017
+        ////Vikrant - 26/04/2017
+        ////Created to check whether log in user is valid or not
+        //[WebMethod]
+        //public void CheckValidUser(string sUserName,string sPassword)
+        //{
+        //    string sResult = string.Empty;
+        //    int iValid = 0;
+        //    try
+        //    {
+        //        sFuncName = "CheckValidUser";
+        //        oLog.WriteToDebugLogFile("Validate Login Function", sFuncName);
+
+        //        sSQL = string.Format("call \"AE_SP002_VALIDUSER\"('" + sUserName + "','" + sPassword + "')");
+
+        //        oLog.WriteToDebugLogFile("Execute SQL" + sSQL, sFuncName);
+        //        OdbcParameter[] Param = new OdbcParameter[0];
+
+        //        iValid = oCommon.ExecuteNonQuery_DR(sSQL, Param);
+                
+        //        if(iValid == 1)
+        //        {
+        //            sResult = "SUCCESS";
+        //        }
+        //        else if (iValid == 2)
+        //        {
+        //            sResult = "INCORRECT USERNAME";
+        //        }
+        //        else if (iValid == 3)
+        //        {
+        //            sResult = "INCORRECT PASSWORD";
+        //        }
+        //        else 
+        //        {
+        //            sResult = "FAILURE";
+        //        }
+
+        //        oLog.WriteToDebugLogFile("Ending Function", sFuncName);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        sErrDesc = ex.Message.ToString();
+        //        oLog.WriteToErrorLogFile(sErrDesc, sFuncName);
+        //        oLog.WriteToDebugLogFile("Completed With ERROR  ", sFuncName);
+        //        result objResult = new result();
+        //        objResult.Result = "Error";
+        //        objResult.DisplayMessage = sErrDesc;
+        //        lstResult.Add(objResult);
+        //        Context.Response.Output.Write(js.Serialize(lstResult));
+        //    }
+
+        //    return sResult;
+        //}
+
+        //Vivek - 15/05/2017
         //Created to check whether log in user is valid or not
         [WebMethod]
-        public string CheckValidUser(string sUserName, string sPassword)
+        public void CheckValidUser(string sJsonInput)
         {
             string sResult = string.Empty;
+            result objResult = new result();
             int iValid = 0;
             try
             {
+                string sUserName = string.Empty;
+                string sPassword = string.Empty;
+
                 sFuncName = "CheckValidUser";
                 oLog.WriteToDebugLogFile("Validate Login Function", sFuncName);
-                
+
+                sJsonInput = "[" + sJsonInput + "]";
+                oLog.WriteToDebugLogFile("Getting the Json Input 1 from web  '" + sJsonInput + "'", sFuncName);
+                DataTable dtInput = JsonStringToDataTable(sJsonInput);
+                if (dtInput != null && dtInput.Rows.Count > 0)
+                {
+                    sUserName = dtInput.Rows[0]["sUserName"].ToString();
+                    sPassword = dtInput.Rows[0]["sPassword"].ToString();
+                }
+
                 sSQL = string.Format("call \"AE_SP002_VALIDUSER\"('" + sUserName + "','" + sPassword + "')");
 
                 oLog.WriteToDebugLogFile("Execute SQL" + sSQL, sFuncName);
                 OdbcParameter[] Param = new OdbcParameter[0];
 
                 iValid = oCommon.ExecuteNonQuery_DR(sSQL, Param);
-                
-                if(iValid == 1)
+
+                if (iValid == 1)
                 {
                     sResult = "SUCCESS";
                 }
@@ -121,26 +187,28 @@ namespace AE_OutletManagerService_V001
                 {
                     sResult = "INCORRECT PASSWORD";
                 }
-                else 
+                else
                 {
                     sResult = "FAILURE";
                 }
 
                 oLog.WriteToDebugLogFile("Ending Function", sFuncName);
+                oLog.WriteToDebugLogFile("Completed With SUCCESS ", sFuncName);
+                objResult.Result = "SUCCESS";
+                objResult.DisplayMessage = "Delivery Order Completed successfully";
+                lstResult.Add(objResult);
+                Context.Response.Output.Write(js.Serialize(lstResult));
             }
             catch (Exception ex)
             {
                 sErrDesc = ex.Message.ToString();
                 oLog.WriteToErrorLogFile(sErrDesc, sFuncName);
                 oLog.WriteToDebugLogFile("Completed With ERROR  ", sFuncName);
-                result objResult = new result();
                 objResult.Result = "Error";
                 objResult.DisplayMessage = sErrDesc;
                 lstResult.Add(objResult);
                 Context.Response.Output.Write(js.Serialize(lstResult));
             }
-
-            return sResult;
         }
 
         //Vikrant - 03/05/2017
@@ -203,5 +271,102 @@ namespace AE_OutletManagerService_V001
         }
 
         #endregion
+
+        #region Public Methods
+        public DataTable JsonStringToDataTable(string jsonString)
+        {
+            DataTable dt = new DataTable();
+            string sFuncName = string.Empty;
+            try
+            {
+                sFuncName = "JsonStringToDataTable()";
+                oLog.WriteToDebugLogFile("Starting Function ", sFuncName);
+
+                string[] jsonStringArray = Regex.Split(jsonString.Replace("[", "").Replace("]", ""), "},{");
+                if (jsonStringArray[0].ToString() != string.Empty)
+                {
+                    List<string> ColumnsName = new List<string>();
+                    foreach (string jSA in jsonStringArray)
+                    {
+                        string sjSA = jSA;
+                        if (jSA.Contains("base64,"))
+                        {
+                            sjSA = jSA.Replace("base64,", "base64;");
+                        }
+                        string[] jsonStringData = Regex.Split(sjSA.Replace("{", "").Replace("}", ""), "\",");
+                        foreach (string ColumnsNameData in jsonStringData)
+                        {
+                            try
+                            {
+                                int idx = ColumnsNameData.IndexOf(":");
+                                string ColumnsNameString = ColumnsNameData.Substring(0, idx - 1).Replace("\"", "");
+                                if (!ColumnsName.Contains(ColumnsNameString.Trim()))
+                                {
+
+                                    ColumnsName.Add(ColumnsNameString.Trim());
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                throw new Exception(string.Format("Error Parsing Column Name : {0}", ColumnsNameData));
+                            }
+                        }
+                        break;
+                    }
+                    foreach (string AddColumnName in ColumnsName)
+                    {
+                        if (AddColumnName.Contains("Date"))
+                        { dt.Columns.Add(AddColumnName, typeof(DateTime)); }
+                        else
+                        { dt.Columns.Add(AddColumnName); }
+
+                    }
+                    foreach (string jSA in jsonStringArray)
+                    {
+                        string sjSA = jSA;
+                        if (jSA.Contains("base64,"))
+                        {
+                            sjSA = jSA.Replace("base64,", "base64;");
+                        }
+                        string[] RowData = Regex.Split(sjSA.Replace("{", "").Replace("}", ""), "\",");
+                        DataRow nr = dt.NewRow();
+                        foreach (string rowData in RowData)
+                        {
+                            try
+                            {
+                                string RowDataString = string.Empty;
+                                int idx = rowData.Trim().IndexOf(":");
+                                string RowColumns = rowData.Trim().Substring(0, idx - 1).Replace("\"", "");
+                                if (rowData.Trim().Substring(idx + 1).Replace("\"", "").Contains("base64;"))
+                                {
+                                    RowDataString = rowData.Trim().Substring(idx + 1).Replace("\"", "").Replace("base64;", "base64,");
+                                }
+                                else
+                                {
+                                    RowDataString = rowData.Trim().Substring(idx + 1).Replace("\"", "");
+                                }
+
+                                nr[RowColumns] = RowDataString.Trim();
+
+                            }
+                            catch (Exception ex)
+                            {
+                                continue;
+                            }
+                        }
+                        dt.Rows.Add(nr);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                sErrDesc = ex.Message.ToString();
+                oLog.WriteToErrorLogFile(sErrDesc, sFuncName);
+                oLog.WriteToDebugLogFile("Completed With ERROR  ", sFuncName);
+            }
+            return dt;
+        }
+
+        #endregion Public Methods
     }
 }
